96b41ce565d9c81585fa6afe5aa128b5da3c2be3
ei-sei <sk.ahm2019@gmail.com>
2026-05-30 16:04:41 +0100

    feat: add ecr repository #4

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,3 +5,18 @@ module "network" {
   environment        = var.environment
   name_prefix        = var.name_prefix
 }
+
+resource "aws_ecr_repository" "ecr_repository" {
+  name                 = var.ecr_repository_name
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    name        = var.ecr_repository_name
+    environment = var.environment
+  }
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -26,3 +26,8 @@ variable "name_prefix" {
   description = "Prefix for resource names."
   type        = string
 }
+
+variable "ecr_repository_name" {
+  description = "The name of the ECR repository."
+  type        = string
+}
