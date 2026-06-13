f2b4a074e2620bc4337fd55bbdb0eca14b624c9f
ei-sei <sk.ahm2019@gmail.com>
2026-06-13 19:43:49 +0100

    feat: add github oidc identity provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -40,3 +40,14 @@ module "ecs" {
   tg_wireguard_arn    = module.nlb.tg_wireguard_arn
   depends_on          = [module.network, module.nlb, module.ecr]
 }
+
+
+data "tls_certificate" "github" {
+  url = "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
+}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
+}
