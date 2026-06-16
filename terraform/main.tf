a5af564ee3f3a9d2eff925dd5e4932c60374e87b
ei-sei <sk.ahm2019@gmail.com>
2026-06-16 18:49:10 +0100

    feat: add permissions policy for github actions deploy role

--- terraform/main.tf
+++ terraform/main.tf
@@ -82,3 +82,40 @@ resource "aws_iam_role" "github_actions_deploy" {
   assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
 }
 
+# Permissions policy for GitHub Actions to deploy to ECS
+data "aws_iam_policy_document" "github_actions_deploy_permissions" {
+  statement {
+    sid       = "ECRAuth"
+    actions   = ["ecr:GetAuthorizationToken"]
+    resources = ["*"]
+  }
+
+  statement {
+    sid = "ECRPush"
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ecr:PutImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+    ]
+    resources = [module.ecr.ecr_repository_arn]
+  }
+
+  statement {
+    sid = "ECSDeploy"
+    actions = [
+      "ecs:UpdateService",
+      "ecs:DescribeServices",
+    ]
+    resources = ["arn:aws:ecs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:service/${var.name_prefix}-cluster/${var.name_prefix}-service"]
+  }
+}
+
+resource "aws_iam_role_policy" "github_actions_deploy" {
+  name   = "${var.name_prefix}-github-actions-deploy-policy"
+  role   = aws_iam_role.github_actions_deploy.id
+  policy = data.aws_iam_policy_document.github_actions_deploy_permissions.json
+}
